5ddb90a80d4feb1a834b1397e9ec49842e5863fc
Sebastian Beltran <bjohansebas@gmail.com>
2025-04-03 17:36:35 -0500

    openssl for windows

--- test/support/gencert.sh
+++ test/support/gencert.sh
@@ -1,5 +1,17 @@
 #! /bin/sh
-set -ex
+set -e
+
+# Detect OS
+OS="$(uname -s)"
+
+# Define file paths
+KEY_PATH="./test/fixtures/server.key"
+CERT_PATH="./test/fixtures/server.crt"
+SUBJ="/C=US/ST=Illinois/L=Chicago/O=node-express-session/CN=express-session.local"
+
+if [[ "$OS" == MINGW* || "$OS" == MSYS* ]]; then
+  SUBJ=/"/C=US/ST=Illinois/L=Chicago/O=node-express-session/CN=express-session.local"
+fi
 
 openssl req -x509 -nodes -newkey rsa:2048 -keyout ./test/fixtures/server.key -out ./test/fixtures/server.crt -days 3650 \
--subj "/C=US/ST=Illinois/L=Chicago/O=node-express-session/CN=express-session.local"
+-subj "$SUBJ"
